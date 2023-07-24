c9c36cecdafa30571cc6017696e6bb38cfd115b5
eddvornichenko <127417581+eddvornichenko@users.noreply.github.com>
2023-07-24 12:40:50 +0300

    task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const createGalleryItem = ({ preview, original, description }) => {
+  const galleryItem = document.createElement("li");
+  galleryItem.classList.add("gallery__item");
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.src = preview;
+  galleryImage.alt = description;
+  galleryImage.dataset.source = original;
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+
+  return galleryItem;
+};
+
+const galleryItemsMarkup = galleryItems.map(createGalleryItem);
+gallery.append(...galleryItemsMarkup);
+
+gallery.addEventListener("click", handleGalleryClick);
+
+function handleGalleryClick(event) {
+  event.preventDefault();
+
+  const { target } = event;
+  if (target.nodeName !== "IMG") {
+    return;
+  }
+
+  const largeImageUrl = target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${largeImageUrl}" width="800" height="600">
+  `);
+  instance.show();
+
+  window.addEventListener("keydown", handleKeyPress);
+
+  function handleKeyPress(event) {
+    if (event.code === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", handleKeyPress);
+    }
+  }
+}
