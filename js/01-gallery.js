1597c6cacae9e996dc4d587e0f9fe4a80b90cab5
eddvornichenko <127417581+eddvornichenko@users.noreply.github.com>
2023-07-24 18:22:31 +0300

    update-1,2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,30 +1,22 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
 const gallery = document.querySelector(".gallery");
 
-const createGalleryItem = ({ preview, original, description }) => {
-  const galleryItem = document.createElement("li");
-  galleryItem.classList.add("gallery__item");
-
-  const galleryLink = document.createElement("a");
-  galleryLink.classList.add("gallery__link");
-  galleryLink.href = original;
-
-  const galleryImage = document.createElement("img");
-  galleryImage.classList.add("gallery__image");
-  galleryImage.src = preview;
-  galleryImage.alt = description;
-  galleryImage.dataset.source = original;
-
-  galleryLink.appendChild(galleryImage);
-  galleryItem.appendChild(galleryLink);
-
-  return galleryItem;
-};
-
-const galleryItemsMarkup = galleryItems.map(createGalleryItem);
-gallery.append(...galleryItemsMarkup);
+const galleryItem = galleryItems
+  .map(
+    (item) => `<li class="gallery__item">
+<a class="gallery__link" href="${item.original}">
+  <img
+    class="gallery__image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}"
+  />
+</a>
+</li>`
+  )
+  .join("");
+gallery.innerHTML = galleryItem;
 
 gallery.addEventListener("click", handleGalleryClick);
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,30 +2,21 @@ import { galleryItems } from "./gallery-items.js";
 
 const gallery = document.querySelector(".gallery");
 
-// Create and render gallery markup based on galleryItems
-function renderGallery() {
-  const galleryMarkup = galleryItems
-    .map(
-      (item) =>
-        `<li class="gallery__item">
+const galleryMarkup = galleryItems
+  .map(
+    (item) =>
+      `<li class="gallery__item">
            <a class="gallery__link" href="${item.original}">
               <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
            </a>
          </li>`
-    )
-    .join("");
+  )
+  .join("");
 
-  gallery.innerHTML = galleryMarkup;
-}
+gallery.innerHTML = galleryMarkup;
 
-// Initialize SimpleLightbox
-function initLightbox() {
-  new SimpleLightbox(".gallery a", {
-    captionsData: "alt",
-    captionDelay: 250,
-    scrollZoom: false,
-  });
-}
-
-renderGallery();
-initLightbox();
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  scrollZoom: false,
+});
